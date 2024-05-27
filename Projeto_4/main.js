b8a67d65e56470dbc62ccc36bd87a0d399429499
Pamela <pamela_borges18@hotmail.com>
2024-05-27 14:00:45 -0300

    Projeto_4

--- Projeto_4/main.js
+++ Projeto_4/main.js
@@ -1,10 +1,10 @@
-let firstName = prompt("Digite o seu nome")
+// let firstName = prompt("Digite o seu nome")
 console.log(firstName)
 
-let lasttName = prompt("Digite o seu sobrenome")
+// let lasttName = prompt("Digite o seu sobrenome")
 console.log(lasttName)
 
-let age = parseInt(prompt("Digite sua idade"))
+//let age = parseInt(prompt("Digite sua idade"))
 
 // Nao pode ser alterado 
 const age2 = 2
